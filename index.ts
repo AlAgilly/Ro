440f39087e64d6f4d693d97ec35a752aab9a62e1
AlAgilly <alagilly@my.yorku.ca>
2021-10-29 20:01:38 -0400

    Completely functional subtract command and added a features folder

--- index.ts
+++ index.ts
@@ -1,13 +1,15 @@
-import DiscordJS, { Intents, Interaction } from 'discord.js'
+import DiscordJS, { Intents } from 'discord.js'
 import WOKCommands from 'wokcommands'
 import path from 'path'
+// import { resolve } from 'path/posix'
 import dotenv from 'dotenv'
 dotenv.config()
 
 const client = new DiscordJS.Client({
     intents: [
         Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGE_REACTIONS
     ]
 })
 
@@ -16,6 +18,7 @@ client.on('ready', () => {
 
     new WOKCommands(client, {
         commandDir: path.join(__dirname, 'commands'),
+        featureDir: path.join(__dirname, 'features'),
         typeScript: true,
         testServers: ['874715472736682064']
     })
